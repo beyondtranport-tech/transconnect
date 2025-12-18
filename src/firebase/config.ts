be18763a220fd85bd0bd4d5df144592f6bddca90
Michael Koton <beyondtranport@gmail.com>
2025-12-18 10:11:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-743387791-fb66d",
+  "appId": "1:388021192158:web:62652994e1fc77429c494f",
+  "apiKey": "AIzaSyAhKC-n7BWyyXzK06nxNnHE9UZ0loz-kyY",
+  "authDomain": "studio-743387791-fb66d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "388021192158"
+};
