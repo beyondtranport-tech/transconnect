fa38e930c17a95f055a137e625912cfa9cb886d5
Michael Koton <beyondtranport@gmail.com>
2025-12-24 14:16:59 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/backend/reconciliation/transaction-allocation.tsx
+++ src/app/backend/reconciliation/transaction-allocation.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
