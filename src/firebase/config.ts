4e961d4d93dab04121bbfdd81597a8df9d5c76b9
Michael Koton <beyondtranport@gmail.com>
2025-12-27 20:18:55 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "transconnect-v1-39578841-2a857",
   "appId": "1:591619429510:web:5b08ce7c6c6436ef5ced07",
+  "storageBucket": "transconnect-v1-39578841-2a857.appspot.com",
   "apiKey": "AIzaSyDT677wYN1FlCesOM7abhnnVk5E1ujsjhc",
   "authDomain": "transconnect-v1-39578841-2a857.firebaseapp.com",
   "measurementId": "",
