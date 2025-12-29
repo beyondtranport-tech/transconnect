bb48f06bfa385c50ff900aa3ef97ca851b4044ff
Michael Koton <beyondtranport@gmail.com>
2025-12-29 19:23:21 +0000

    i asked you to place the Recent Transactions  card below the Recent Fund

--- src/app/account/dashboard.tsx
+++ src/app/account/dashboard.tsx
@@ -178,13 +178,9 @@ export default function AccountDashboard() {
                 </Card>
             </div>
             
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                <div className="lg:col-span-1">
-                    <RecentEnquiries />
-                </div>
-                <div className="lg:col-span-2">
-                    <RecentTransactions />
-                </div>
+            <div className="space-y-8">
+                <RecentEnquiries />
+                <RecentTransactions />
             </div>
         </div>
     );
