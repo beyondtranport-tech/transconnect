53561d59da71b07c20ecf8cd0813529dffb2325c
Michael Koton <beyondtranport@gmail.com>
2025-12-29 19:20:43 +0000

    i see you are still confused. Let me help you by providing explicate ins

--- src/app/account/dashboard.tsx
+++ src/app/account/dashboard.tsx
@@ -179,11 +179,11 @@ export default function AccountDashboard() {
             </div>
             
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                 <div className="lg:col-span-1">
-                    <RecentTransactions />
+                <div className="lg:col-span-1">
+                    <RecentEnquiries />
                 </div>
                 <div className="lg:col-span-2">
-                    <RecentEnquiries />
+                    <RecentTransactions />
                 </div>
             </div>
         </div>
