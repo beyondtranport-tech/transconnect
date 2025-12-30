e9237300811af59df7128736a30903c013f262ba
Michael Koton <beyondtranport@gmail.com>
2025-12-30 14:16:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/funding/apply/page.tsx
+++ src/app/funding/apply/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Suspense, useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
