4e5c90f61e12433d01173255dc39bee9b333091b
Michael Koton <beyondtranport@gmail.com>
2025-12-30 20:16:54 +0000

    it says Admin SDK Initialized Successfully. what are you doing?????

--- src/next.config.ts
+++ src/next.config.ts
@@ -29,13 +29,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
-    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
-  },
   devServer: {
     allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
