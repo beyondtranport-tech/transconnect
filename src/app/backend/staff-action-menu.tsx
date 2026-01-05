8a083af27da426638f20a265fab6966c0dccbfc3
Michael Koton <beyondtranport@gmail.com>
2026-01-05 15:32:02 +0000

    now /backend?view=staff is displaying html

--- src/app/backend/staff-action-menu.tsx
+++ src/app/backend/staff-action-menu.tsx
@@ -200,7 +200,7 @@ export default function StaffActionMenu({ staffMember, onUpdate }: { staffMember
                     <Eye className="mr-2 h-4 w-4" /> View Company
                 </Link>
              </DropdownMenuItem>
-             <DropdownMenuItem onSelect={() => setIsEditOpen(true)}>
+             <DropdownMenuItem onSelect={(e) => { e.preventDefault(); setIsEditOpen(true); }}>
                 <Edit className="mr-2 h-4 w-4" /> Edit
              </DropdownMenuItem>
             {staffMember.status !== 'confirmed' ? (
