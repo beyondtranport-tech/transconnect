142842700424566e5fbfb5ace001bdd363fc6815
Michael Koton <beyondtranport@gmail.com>
2026-01-07 19:36:50 +0000

    i tried to sign into by backend.. I got in but error is still there. Als

--- src/app/app-provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { FirebaseClientProvider } from '@/firebase/client-provider';
-
-export function AppProvider({ children }: { children: React.ReactNode }) {
-  return <FirebaseClientProvider>{children}</FirebaseClientProvider>;
-}
--- src/app/backend/layout.tsx
+++ src/app/backend/layout.tsx
@@ -1,10 +1,7 @@
-'use client';
-import { AppProvider } from '../app-provider';
-
 export default function BackendLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <AppProvider>{children}</AppProvider>;
+  return <>{children}</>;
 }
