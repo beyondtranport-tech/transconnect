880860529fbff051ebdcc47003d8a9560993768a
Michael Koton <beyondtranport@gmail.com>
2026-01-07 18:19:52 +0000

    ## Error Type Runtime TypeError
    ## Error Message
    (0 , _firebase__WEBPAC

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,10 @@
 
 'use client';
 
-import { TooltipProvider } from '@/components/ui/tooltip';
-
 export function AppProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-      <TooltipProvider>{children}</TooltipProvider>
-  );
+  return <>{children}</>;
 }
