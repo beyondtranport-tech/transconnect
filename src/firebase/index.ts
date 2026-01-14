c92c4c80affa57758d98b1a5c0ed2c75a376a90f
Michael Koton <beyondtranport@gmail.com>
2026-01-14 14:34:00 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -58,5 +58,4 @@ export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './errors';
 export * from './error-emitter';
-export * from '@/hooks/use-config';
     
