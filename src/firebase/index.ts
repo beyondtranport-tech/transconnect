f26a50761c2f88bbd4c564386a18e0fe4f7c0f99
Michael Koton <beyondtranport@gmail.com>
2026-01-23 07:29:49 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,35 +1,7 @@
 
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-
-// IMPORTANT: DO NOT MODIFY THIS FUNCTION
-export function initializeFirebase() {
-  let firebaseApp;
-  if (!getApps().length) {
-    try {
-      firebaseApp = initializeApp(firebaseConfig);
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-  } else {
-    firebaseApp = getApp();
-  }
-
-  const auth = getAuth(firebaseApp);
-  const firestore = getFirestore(firebaseApp);
-  const storage = getStorage(firebaseApp); 
-
-  return { firebaseApp, auth, firestore, storage };
-}
-
+export * from './init';
 export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
