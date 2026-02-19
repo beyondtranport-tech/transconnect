fb19c7c9666234939926f6f89699b802646c26aa
Michael Koton <mkoton100@gmail.com>
2026-02-19 17:26:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@opentelemetry/instrumentation'],
+  },
   images: {
     remotePatterns: [
       {
