da348235b6714c090face52efe71d11f137eca40
Michael Koton <mkoton100@gmail.com>
2026-02-25 14:30:48 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: The default expo

--- src/app/lending/page.tsx
+++ src/app/lending/page.tsx
@@ -1,1 +1,6 @@
-// This file is pending deletion and has been emptied as part of a rollback.
+'use client';
+
+export default function LendingPage() {
+  // This page is a temporary placeholder to fix a build error and will be removed.
+  return null;
+}
