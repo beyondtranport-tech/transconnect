fbb521de815dd170ee4927cb1419d97671ada7ee
Michael Koton <mkoton100@gmail.com>
2026-02-26 10:06:56 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: CardFoo

--- src/app/mall/wcta/page.tsx
+++ src/app/mall/wcta/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { ArrowRight, CheckCircle, Handshake } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
