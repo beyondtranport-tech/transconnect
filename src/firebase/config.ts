c684c359ce971b7545da1be0f85baa54272b3f7b
Michael Koton <mkoton100@gmail.com>
2026-03-01 10:06:39 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "projectId": "ecosystem-hub",
   "appId": "1:204859518196:web:1e61ab7520aa2386c742a1",
   "apiKey": "AIzaSyBdxck-9ewVbdGtR7DuC5GHElv9UfTDL3I",
-  "authDomain": "6000-firebase-studio-1765906568345.cluster-a3jr4qtr75cgqwguxonos5gtfy.cloudworkstations.dev",
+  "authDomain": "ecosystem-hub.firebaseapp.com",
   "measurementId": "G-M2EZDDJ44Y",
   "messagingSenderId": "204859518196"
 };
