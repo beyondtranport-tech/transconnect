e2b66362e24ece03cfaeda5da43472a9f22b051b
Michael Koton <mkoton100@gmail.com>
2026-03-01 10:43:19 +0000

    i tried again, same toast error.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "projectId": "ecosystem-hub",
   "appId": "1:204859518196:web:1e61ab7520aa2386c742a1",
   "apiKey": "AIzaSyBdxck-9ewVbdGtR7DuC5GHElv9UfTDL3I",
-  "authDomain": "ecosystem-hub.firebaseapp.com",
+  "authDomain": "ecosystem-hub.web.app",
   "measurementId": "G-M2EZDDJ44Y",
   "messagingSenderId": "204859518196"
 };
