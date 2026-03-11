15bddbf58d9f33b7ca0210cd94dcdb74dfde85be
Michael Koton <mkoton100@gmail.com>
2026-03-11 10:09:01 +0000

    ok must i try again?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Forcing yet another server restart to clear a corrupted Next.js cache.
+  // Forcing one last, definitive server restart to clear any and all corrupted Next.js cache.
   /* config options here */
   images: {
     remotePatterns: [
