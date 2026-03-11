47be9197fa1c25e833330d6994b8741b528ac315
Michael Koton <mkoton100@gmail.com>
2026-03-11 09:28:08 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Missing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // This comment forces a server restart to clear the cache and fix styling. Please do not remove.
+  // This comment is added to force a server restart and clear the build cache. Please do not remove.
   /* config options here */
   images: {
     remotePatterns: [
