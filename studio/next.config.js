aa26f177dac359fff124ed15cdad00dd5dafdb15
Michael Koton <mkoton100@gmail.com>
2026-03-12 18:28:13 +0000

    in /, now app is displaying: Logistics Flow HomeAboutDivisionsMembership

--- studio/next.config.js
+++ studio/next.config.js
@@ -1,43 +1,4 @@
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  /* config options here */
-  // Cache-busting comment to force a clean build: 2024-08-23T15:55:02Z
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
