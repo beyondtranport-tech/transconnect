f719f66d6af23e8ac966fd086f0450dd90340a56
Michael Koton <mkoton100@gmail.com>
2026-05-12 10:31:03 +0000

    we are back to the previous error: Configuration Issue Detected UNKNOWN:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { useUser } from "@/firebase";
 import * as React from "react";
 import { useState } from "react";
 import * as gtag from '@/lib/gtag';
-import { HomeIntentModal } from "./home-intent-modal";
+import { HomeIntentModal } from "@/app/home-intent-modal";
 
 const { placeholderImages } = data;
 
